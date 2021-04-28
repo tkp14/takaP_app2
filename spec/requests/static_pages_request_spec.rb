e0dcd9ec4020d607ba372cadc597d65429f437a6
tkp14 <tk.091438@gmail.com>
2021-04-28 10:09:04 +0900

    リクエストテスト作成2

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -18,4 +18,12 @@ RSpec.describe "静的ページ", type: :request do
     end
   end
 
+  describe "アバウトページ" do
+    it "正常なレスポンスを返すこと" do
+      get about_path
+      expect(response).to be_success
+      expect(response).to have_http_status "200"
+    end
+  end
+
 end
