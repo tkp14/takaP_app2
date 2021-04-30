dc2ef215b9d4a1a3d611135d31c34812532cfc37
tkp14 <tk.091438@gmail.com>
2021-04-30 23:43:06 +0900

    Userモデル バリデーションの設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,10 +14,22 @@ RSpec.describe User, type: :model do
       expect(user.errors[:name]).to include("を入力してください")
     end
 
+    it "名前が50文字以内であること" do
+      user = build(:user, name: "a" * 51)
+      user.valid?
+      expect(user.errors[:name]).to include("は50文字以内で入力してください")
+    end
+
     it "メールアドレスがなければ無効な状態であること" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "メールアドレスが255文字以内であること" do
+      user = build(:user, email: "a" * 255)
+      user.valid?
+      expect(user.errors[:email]).to include("は255文字以内で入力してください")
     end
   end
 end
