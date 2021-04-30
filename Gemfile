f8321cc657c4baefba2a52569ffd85e862cd7fb5
tkp14 <tk.091438@gmail.com>
2021-04-30 19:33:50 +0900

    Userモデルのテストの準備、作成

--- Gemfile
+++ Gemfile
@@ -13,11 +13,13 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'faker'
 
 group :development, :test do
   gem 'rspec-rails', '~> 4.0.0.beta2'
   gem 'rails-controller-testing'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "factory_bot_rails", "~> 4.10.0"
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  context "バリデーション" do
+    it "名前、メールアドレスがあれば有効な状態であること" do
+      expect(user).to be_valid
+    end
+
+    it "名前がなければ無効な状態であること" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "メールアドレスがなければ無効な状態であること" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+  end
 end
