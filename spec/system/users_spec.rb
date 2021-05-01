35d7a67aeacaf0992bbc4542e48a8e9dd132c26c
tkp14 <tk.091438@gmail.com>
2021-05-02 00:37:53 +0900

    リクエスト、システムテスト記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,14 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+
   describe "ユーザー登録ページ" do
-    context "ページ全体" do
-      before do
-        visit signup_path
+    before do
+      visit signup_path
+    end
+
+    context "ページレイアウト" do
+      it "「Sign up」の文字列が存在することを確認" do
+        expect(page).to have_content 'Sign up'
       end
 
       it "正しいタイトルが表示されることを確認" do
-        expect(page).to have_title full_title
+        expect(page).to have_title full_title('Sign up')
+      end
+    end
+
+    context "ユーザー登録処理" do
+      it "有効なユーザーでユーザー登録を行うとユーザー登録成功のフラッシュが表示されること" do
+        fill_in "ユーザー名", with: "Example User"
+        fill_in "メールアドレス", with: "user@example.com"
+        fill_in "パスワード", with: "foobar"
+        fill_in "パスワード(確認)", with: "foobar"
+        click_button 'Create my account'
+        expect(page).to have_content "Welcome to the Sample App!"
+      end
+
+      it "無効なユーザーでユーザー登録を行うと登録失敗のフラッシュが表示されること" do
+        fill_in "ユーザー名", with: ""
+        fill_in "メールアドレス", with: "user@example.com"
+        fill_in "パスワード", with: "foobar"
+        fill_in "パスワード(確認)", with: "foobar"
+        click_button 'Create my account'
+        expect(page).to have_content "ユーザー名を入力してください"
+      end
+    end
+  end
+
+  describe "プロフィールページ" do
+    context "ページレイアウト" do
+      before do
+        visit user_path(user)
+      end
+
+      # it "正しいタイトルが表示されることを確認" do
+      #   expect(page).to have_title full_title('プロフィール')
+      # end
+
+      it "ユーザー情報が表示されることを確認" do
+        expect(page).to have_content user.name
       end
     end
   end
