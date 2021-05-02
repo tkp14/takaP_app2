6fd2753c6de44389fe04fcafa62211fbd8a77a48
tkp14 <tk.091438@gmail.com>
2021-05-02 15:25:53 +0900

    ログインページ作成

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
+RSpec.describe "ログイン機能", type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/sessions/new"
-      expect(response).to have_http_status(:success)
+  describe "ログインページ" do
+    it "正常なレスポンスを返すこと" do
+      get login_path
+      expect(response).to be_success
+      expect(response).to have_http_status "200"
     end
   end
-
 end
