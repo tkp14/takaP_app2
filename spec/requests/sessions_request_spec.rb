b4e6afb938043ab87f62e588657e8d872832f29f
tkp14 <tk.091438@gmail.com>
2021-05-03 17:55:35 +0900

    テスト記述

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "ログイン機能", type: :request do
+  let!(:user) { create(:user) }
 
   describe "ログインページ" do
     it "正常なレスポンスを返すこと" do
       get login_path
       expect(response).to be_success
       expect(response).to have_http_status "200"
+    end
+
+    it "有効なユーザーでログイン&ログアウト" do
+      get login_path
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password } }
+      redirect_to user
+      follow_redirect!
+      expect(response).to render_template('users/show')
+      expect(is_logged_in?).to be_truthy  #ログインしている状態を確認
+      delete logout_path
+      expect(is_logged_in?).not_to be_truthy  #ログインしていない状態を確認
+      redirect_to root_url
+    end
+
+    it "無効なユーザーでログイン" do
+      get login_path
+      post login_path, params: { session: { email: "user@example.com",
+                                           password: "pass" } }
+      expect(is_logged_in?).not_to be_truthy
     end
   end
 end
--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :system do
-  let!(:user) { create(:user) }
+  describe "ログインについて" do
+    let!(:user) { create(:user) }
 
-  before do
-    visit login_path
-  end
+    before do
+      visit login_path
+    end
 
-  describe "ログインページ" do
     context "ページレイアウト" do
       it "正しいタイトルが表示されることを確認" do
         expect(page).to have_title full_title('Login')
@@ -23,8 +23,7 @@ RSpec.describe "Sessions", type: :system do
       end
 
       it "ログインフォームが正しく表示される" do
-        expect(page).to have_css 'input#user_email'
-        expect(page).to have_css 'input#user_password'
+        expect(page).to have_css 'input.form-control'
       end
 
       it "ログインボタンが表示されていること" do
@@ -34,13 +33,34 @@ RSpec.describe "Sessions", type: :system do
 
     context "ログイン処理" do
       it "無効なユーザーの場合はログインが失敗すること" do
-        fill_in "メールアドレス", with: "user@example.com"
-        fill_in "パスワード", with: "foo"
+        fill_in "user_email", with: user.email
+        fill_in "user_password", with: "pass"
         click_button 'ログイン'
         expect(page).to have_content 'Invalid email/password combination'
 
         visit root_path
         expect(page).not_to have_content 'Invalid email/password combination'
+      end
+
+      it "有効なユーザーの場合はログインが成功し、ヘッダーメニューが変わること" do
+        expect(page).to have_link 'Home', href: root_path
+        expect(page).to have_link 'Help', href: help_path
+        expect(page).to have_link 'About', href: about_path
+        expect(page).to have_link 'Contact', href: contact_path
+        expect(page).to have_link 'Log in', href: login_path
+
+        fill_in "user_email", with: user.email
+        fill_in "user_password", with: user.password
+        click_button 'ログイン'
+
+        expect(page).to have_link 'Home', href: root_path
+        expect(page).to have_link 'Help', href: help_path
+        expect(page).to have_link 'About', href: about_path
+        expect(page).to have_link 'Users', href: root_path
+
+       # expect(page).to have_link 'Profile', href: current_user
+
+        expect(page).to have_link 'Log out', href: logout_path
       end
     end
   end
