e1c1114f15fd1845bf800da719253c742bdc155b
tkp14 <tk.091438@gmail.com>
2021-05-05 01:30:11 +0900

    フレンドリーフォワーディングの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+  
+
   def show
     @user = User.find(params[:id])
   end
@@ -15,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_back_or user
     else
       render 'new'
     end
@@ -44,6 +46,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
