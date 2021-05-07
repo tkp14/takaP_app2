fe8d91e3bc391e95ebdc0012ae56003e4bc1a4ed
tkp14 <tk.091438@gmail.com>
2021-05-08 00:00:59 +0900

    Userメイラーの生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
-      self.email = email.downcase
+      email.downcase!
     end
 
     # 有効化トークンとダイジェストを作成および代入する
