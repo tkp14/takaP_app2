a4bc41b3732e9769d43f20f46a320986a06ecbd8
tkp14 <tk.091438@gmail.com>
2021-05-08 21:30:59 +0900

    パスワードの再設定ができるようになった

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,6 +61,11 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago #2時間以上前 (早い) の場合
+  end
+
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
