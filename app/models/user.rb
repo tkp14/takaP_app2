30fce09d4336047bf45fe3ee9c607a77e6f6ff14
tkp14 <tk.091438@gmail.com>
2021-05-09 17:40:39 +0900

    micropostの投稿、削除の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago #2時間以上前 (早い) の場合
   end
 
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
